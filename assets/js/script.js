9fc19f1a303e8729c3788d021d9ffb6663e38ec9
Brett Miller <brett.miller47@yahoo.com>
2022-08-11 16:33:05 -0400

    1) added link to bootstrap 2) created dictionary with four questions, their options, their answer, and their explanation

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+var questions = {
+    "Question_1": {
+        "Qeustion": "Pseudo -elements such as ::before and ::after are part of the DOM.",
+        "Options": ["True", "False"],
+        "Answer": "False",
+        "Explanation": "The DOM is built from the source HTML document alone, and the pseudo-elements are part of the CSSOM and render tree."
+    },
+    "Question_2": {
+        "Question": "The tag <h1> is referred to as which of the following:",
+        "Options": ["element", "class", "attribute", "none of the above"],
+        "Answer": "True",
+        "Explanation": "HTML tags are elements."
+    },
+    "Question_3": {
+        "Question": "The hashtag (#) symbol is used to select which attribute in css?",
+        "Options": ["class", "ID", "data", "style"],
+        "Answer": "ID",
+        "Explanation": "The # symbol is used for ID attributes."
+    },
+    "Question_4": {
+        "Question": "In css, a period (.) is used before a word to select a...",
+        "Options": ["class", "ID", "data", "style"],
+        "Answer": "class",
+        "Explanation": "The . symbol is used for classes."
+    }
+}
